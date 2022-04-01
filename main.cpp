4781fc95ac5227274ad87a9ab37b759370ce9bb6
sumov <sumover@foxmail.com>
2022-04-01 19:40:28 +0800

    随便写几个测试玩玩.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
+#include "jsonStruct/JSONObject.h"
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("type --help to show more information.");
-        return 0;
-    } else {
-    }
+
+    JSONObject jsonObject;
+
 }
